202122313f70e697d575455cd800f1f7d44615c0
MissDine <enidnyatichi1212@gmail.com>
2022-05-29 15:09:16 +0300

    Feat:Edit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 //Conditions
-function isEnid(cat){
-    if(cat === "Enid"){
-        console.log("Hello");
-        return
-            "Hi Enid you look beautiful"
-    }else{
-        return(
-            `Hi ${cat}`
-        )
-    }
+let num1 = 11;
+let num2 = 11;
+let result = num2 > num1
+if(num2 >num1){
+    console.log("Num 2 is greater than num1")
+}else if(num1 > num2){
+    console.log("Num1 is greater than num2");
+}else{
+    console.log("They are equal");
 }
 
+//
