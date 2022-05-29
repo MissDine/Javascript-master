633647424affbcd41d47d988eac0fd754fb58cd2
MissDine <enidnyatichi1212@gmail.com>
2022-05-29 15:48:22 +0300

    Feat:Edit

--- index.js
+++ index.js
@@ -16,5 +16,15 @@ points = namee === "Enidd"?"You are right" :"You are wrong"
 console.log(points)
 
 {
-    const age = prompt("How old am I?");age ===15?console.log("10"):console.log("0");
-}    
+    const age = prompt("How old am I?");
+    age ==="10"?console.log("10"):console.log("0");
+}
+{
+    const love = prompt("What is love?")
+    inove =love==="1"?"You are in love!":"You are not in love!"
+} 
+console.log(inove);
+
+const tribe = prompt("Guess my tribe")
+tell = tribe==="Kisii"?"Right":"Guess again"
+console.log(tell);
